de2f7393d0f708d7c9f47bbf1e3d986386a3ff0c
Aelmmmson <andersonbenjamin98@outlook.com>
2026-08-12 16:24:56 +0000

    New Origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -6,6 +6,7 @@ const allowedOrigins = [
     'http://10.203.14.73:3000',
     'http://192.168.1.142:8080',
     'http://localhost:8046',
+    'http://10.203.14.169:8046'
 ]
 
 module.exports = allowedOrigins;
